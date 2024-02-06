e279a99291aa26ee1ad2ec0a6e95dd8b51df2810
KenwoodFox <kenwood364@gmail.com>
2024-02-06 13:02:09 -0500

    Add some new features to the fanclub cog

--- fops_bot/cogs/fanclub.py
+++ fops_bot/cogs/fanclub.py
@@ -43,17 +43,51 @@ class FanclubCog(commands.Cog, name="FanclubCog"):
 
         return self.tempList[guild]
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        logging.info(message)
+    @commands.Cog.listener("on_message")
+    async def boopListener(self, message: discord.Message):
+        logging.debug(f"Boop Listener processing {message}")
 
         # Check for boops!
         boops = ["boop", "boops", "boop'd"]
-        if (any(item in message.content for item in boops)) and (
-            message.author != self.bot.user
+        if (any(item in message.content.lower() for item in boops)) and (
+            not message.author.bot
         ):
             logging.debug(f"Boop detected in {message}, guild was {message.guild}")
             await message.reply(f"{self.getStat(message.guild, True)} boops!")
+
+    @commands.Cog.listener("on_message")
+    async def holeInTheWallListener(self, msg: discord.Message):
+        logging.debug(f"Hole In the wall got {msg}")
+
+        chan = int(os.environ.get("HOLE_CHAN", ""))
+        user = int(os.environ.get("HOLE_USER", ""))
+
+        if isinstance(msg.channel, discord.channel.DMChannel):
+            "What to do if dm'd directly!"
+            logging.info(f"Got a dm! {msg.content}")
+
+            holeChan = self.bot.get_channel(chan)
+            await holeChan.send(msg.content)
+            await msg.add_reaction("📬")
+        elif (
+            not msg.author.bot  # Not ourself
+            and msg.channel.id == chan  # and in the right channel
+        ):
+            # Extract the content
+            ct = msg.content
+
+            if ct[0] == "(":
+                # Checks if the message is OOC, and if it is, skips it
+                await msg.add_reaction("🔇")
+            else:
+                # Time to actually forward a message!
+                recipient = await self.bot.fetch_user(user)
+                if recipient == None:
+                    await msg.add_reaction("❌")
+                    return
+                else:
+                    await recipient.send(f"*Somebody..*\n{ct}")
+                    await msg.add_reaction("📬")
 
 
 async def setup(bot):
